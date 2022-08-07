c6e05a53ace9ac2ca6f76b4ece57386e73c2c4cd
Mittelblut9 <sauer.benedikt@outlook.de>
2022-08-07 16:10:51 +0200

    modified:   server/route/noui/changelog-route.js

--- server/route/noui/changelog-route.js
+++ server/route/noui/changelog-route.js
@@ -2,6 +2,6 @@ const nconf = require('nconf');
 
 module.exports = (app) => {
     app.get(nconf.get('routing:changelog'), async (req, res) => {
-        res.redirec(nconf.get('external_links:changelog'));
+        res.redirect(nconf.get('external_links:changelog'));
     });
 }
