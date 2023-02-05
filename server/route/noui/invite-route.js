c2764c7e70a6d3127d5208ac4e3e2b68be8ec9da
Mittelblut9 <info@blackdayz.de>
2023-02-05 15:39:34 +0100

    fix/add invite link

--- server/route/noui/invite-route.js
+++ server/route/noui/invite-route.js
@@ -2,7 +2,6 @@ const nconf = require('nconf');
 
 module.exports = (app) => {
     app.get(nconf.get('routing:invite'), async (req, res) => {
-        return res.send(`Invite link is not available. Please join the discord support server to get access. <a href="${nconf.get('routing:support')}">Click here to join the support server</a>`);
         res.redirect(nconf.get('external_links:invite'))
     });
 }
